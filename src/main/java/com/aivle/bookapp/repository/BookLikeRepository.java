75342116fd94f835fd9f837542104f214ef04a09
YWSIK <yuusik818@gmail.com>
2026-06-09 15:21:44 +0900

    feat: booklike repo

---
+++ src/main/java/com/aivle/bookapp/repository/BookLikeRepository.java
@@ -0,0 +1,17 @@
+package com.aivle.bookapp.repository;
+
+import com.aivle.bookapp.domain.BookLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class BookLikeRepository extends JpaRepository<BookLike, Long> {
+
+    // 특정 유저, 도서 좋아요 조회
+    Optional<BookLike> findByUserAndBook(User user, Book book);
+
+    // 좋아요 클릭 여부 확인
+    boolean existsByUserAndBook(User user, Book book)
+}
