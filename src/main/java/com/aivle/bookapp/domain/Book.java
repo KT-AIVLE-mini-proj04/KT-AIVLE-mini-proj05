9149af17a0967b6194d56a87982568d0d2fd9139
YWSIK <yuusik818@gmail.com>
2026-06-09 15:11:33 +0900

    feat: entity 추가

--- src/main/java/com/aivle/bookapp/domain/Book.java
+++ src/main/java/com/aivle/bookapp/domain/Book.java
@@ -1,5 +1,29 @@
 package com.aivle.bookapp.domain;
 
-public class Book {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
 
+@Entity
+@Getter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private int likeCount = 0;
+
+    public void increaseLikeCount() {
+        this.likeCount++;
+    }
+
+    public void decreaseLikeCount() {
+        if (this.likeCount > 0) {
+            this.likeCount--;
+        }
+    }
 }
---
+++ src/main/java/com/aivle/bookapp/domain/BookLike.java
@@ -0,0 +1,29 @@
+package com.aivle.bookapp.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BookLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    public BookLike(User user, Book book) {
+        this.user = user;
+        this.book = book;
+    }
+}
