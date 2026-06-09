7591689d73243e16e75d16393138d3aca3917aca
Lio <egoing@naver.com>
2026-06-09 18:12:17 +0900

    refactor: 검색키워드 불러오는 기능 수정

--- src/main/java/com/aivle/bookapp/repository/BookRepository.java
+++ src/main/java/com/aivle/bookapp/repository/BookRepository.java
@@ -4,8 +4,13 @@ import com.aivle.bookapp.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository // 이 인터페이스가 데이터베이스 접근자 역할을 함을 명시
+import java.util.List;
+
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    
+    // 검색 기능용: 제목에 특정 단어(keyword)가 포함된 책들을 찾아오는 기능
+    List<Book> findByTitleContaining(String keyword);
     // JpaRepository를 상속받기만 하면 
     // 기본 CRUD(save, findAll, findById, delete) 기능이 자동으로 생성됩니다!
 }
