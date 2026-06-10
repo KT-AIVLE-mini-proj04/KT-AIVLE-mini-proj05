f08a2f7efc72f60754ee091f177421fe7ae0e6a4
jhdgo1225 <parkjh99365@gmail.com>
2026-06-10 14:17:21 +0900

    FEAT: Auth Controller 수정

--- src/main/java/com/aivle/bookapp/controller/UserController.java
+++ src/main/java/com/aivle/bookapp/controller/UserController.java
@@ -1,11 +1,7 @@
 package com.aivle.bookapp.controller;
 
-import com.aivle.bookapp.domain.User;
 import com.aivle.bookapp.dto.users.SignUpRequestDto;
 import com.aivle.bookapp.dto.UserResponseDto;
-import com.aivle.bookapp.dto.users.SignUpResponseDto;
-import com.aivle.bookapp.global.util.Sha256Util;
-import com.aivle.bookapp.repository.UserRepository;
 import com.aivle.bookapp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
