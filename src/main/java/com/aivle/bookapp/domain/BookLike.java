929aa3cb779045be6927890c7e5a819d800d186a
kim <jjang981226@naver.com>
2026-06-11 23:03:17 +0900

    like update

--- src/main/java/com/aivle/bookapp/domain/BookLike.java
+++ src/main/java/com/aivle/bookapp/domain/BookLike.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
