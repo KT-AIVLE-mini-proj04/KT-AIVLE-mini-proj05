4eb038c38ff93ec5d01a8c880f9d8bbe71fa5a20
jhdgo1225 <parkjh99365@gmail.com>
2026-06-12 10:57:02 +0900

    FEAT: 리프레시 토큰 구현 완료

---
+++ src/main/java/com/aivle/bookapp/global/util/RefreshTokenCookieSupport.java
@@ -0,0 +1,9 @@
+package com.aivle.bookapp.global.util;
+
+public final class RefreshTokenCookieSupport {
+
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
+
+    private RefreshTokenCookieSupport() {
+    }
+}
